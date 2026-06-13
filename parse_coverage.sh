8fb49709bfd75053556def194667a4d7de7720e4
Pependr <antonkudrikov88@gmail.com>
2026-06-13 20:33:56 +0300

    Improve parse_coverage.sh

--- parse_coverage.sh
+++ parse_coverage.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 pytest -q --cov --cov-report json
-honk parse -p coverage.json -t $1 -d ${2:-"/"} json | uv run -m json.tool | less
+honk parse -p coverage.json -t $1 -d ${2:-"/"} json  | honk parse -m json group | honk parse -m json map-map --header "file name" >> missinglines.md
 unlink coverage.json
