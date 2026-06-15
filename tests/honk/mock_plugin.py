e790549d5f7fafec85d1a5cc5f2415596d58b583
Pependr <antonkudrikov88@gmail.com>
2026-06-15 11:56:07 +0300

    Extend mock plugin

--- tests/honk/mock_plugin.py
+++ tests/honk/mock_plugin.py
@@ -13,6 +13,21 @@ def txt(data: str) -> str:
     return f"Read text: {data}"
 
 
-@honk.template("mock")
+type NestedList[T] = list[NestedList[T] | T]
+
+
+def parse(data: str) -> NestedList[str] | str:
+    if "," not in data:
+        return data
+    return [parse(block) for block in data.split(",")]
+
+
+@honk.parser("nested")
+def nested(data: str) -> NestedList[str] | str:
+    truncated = data.replace(" ", "").replace("\n", "")
+    return parse(truncated)
+
+
+@honk.template()
 def mock(data: str) -> None:
     click.echo(f"Processed data: {data}")
